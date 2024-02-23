905a563a5665496f49c88a343a50b74f4418af29
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-02-23 13:48:56 +0530

    new design

--- src/Helpers/tokenlist.jsx
+++ src/Helpers/tokenlist.jsx
@@ -1,13 +1,12 @@
-// import BNB from '../images/bnb.png'
+import BNB from '../images/bnb.png'
 import USDT from '../images/usdt.svg'
 export const list = [
   {
+    icon: BNB,
+    name: "BNB",
+  },
+  {
     icon: USDT,
     name: "USDT",
-  }
-  // {
-  //   icon: BNB,
-  //   name: "BNB",
-  // },
-  
+  },
 ];
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../images/logo.png'
+import Logo from '../../images/twkl-logo.png'
 import './style.css'
 import { Web3Button } from '@web3modal/react'
 
